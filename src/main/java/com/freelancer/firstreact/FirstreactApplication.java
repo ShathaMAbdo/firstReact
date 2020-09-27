27c0fe9751c6da887355524d54f7119652807b91
shatha <shatha.m.abdo@gmail.com>
2020-09-27 08:19:47 +0200

    Built project

---
+++ src/main/java/com/freelancer/firstreact/FirstreactApplication.java
@@ -0,0 +1,13 @@
+package com.freelancer.firstreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstreactApplication.class, args);
+	}
+
+}
